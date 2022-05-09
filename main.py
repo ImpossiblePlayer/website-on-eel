c6855b2ac6ad3781336af655d0cc1a45dfaaa500
Ivan <ivanpronin1379@gmail.com>
2022-05-09 23:44:13 +0500

    1

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ data = {}
 date = dt.datetime.now().strftime('%d:%m:%y')
 
 @eel.expose
-def return_data():
+def return_info():
 	with open('data.json', 'r') as file:
 		data = json.load(file)
 	return data
 
 
 @eel.expose
-def add_data(task: str):
+def add_info(task: str):
 	with open('data.json', 'w') as file:
 		json.dump(data, file, indent=2)
 
 eel.init('web')
-eel.start('index.html')
+eel.start('index.html', size=(1100, 900))
 
 
 
